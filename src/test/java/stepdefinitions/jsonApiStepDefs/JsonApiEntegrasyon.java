dd983f97a20b7a6f13b9d306462e6ef90e1b1e40
Recep Aykurt <recep.aykurt7@gmail.com>
2022-05-17 02:06:21 +0300

    "Fourth"

---
+++ src/test/java/stepdefinitions/jsonApiStepDefs/JsonApiEntegrasyon.java
@@ -0,0 +1,44 @@
+package stepdefinitions.jsonApiStepDefs;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.restassured.response.Response;
+import utilities.Log;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+
+public class JsonApiEntegrasyon {
+
+    String endpoint;
+    Response response;
+
+    @Given("kullanici endpoint olarak {string} tanimlar")
+    public void kullanici_endpoint_olarak_tanimlar(String string) {
+        endpoint = string;
+    }
+
+    @Given("kullanici request gönderip response elde eder")
+    public void kullanici_request_gönderip_response_elde_eder() {
+        response = given().when().get(endpoint);
+
+    }
+
+    @Then("kullanici status code assert eder")
+    public void kullanici_status_code_assert_eder() {
+        Log.info("Api Status Code Kontrol Ediliyor...");
+        response.then().statusCode(200);
+    }
+
+    @Then("kullanici clients sayisini assert eder")
+    public void kullanici_clients_sayisini_assert_eder() {
+
+        List<Object> el = response.as(List.class);
+        System.out.println("el = " + el);
+        System.out.println(el.size());
+
+
+    }
+
+}
