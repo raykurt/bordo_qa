5186c49571c40cb7a8803bb5cac7c2a19daa486b
Recep <recep.aykurt7@gmail.com>
2022-08-30 22:10:10 +0300

    "Recep"

--- src/test/java/utilities/Check.java
+++ src/test/java/utilities/Check.java
@@ -3,7 +3,7 @@ package utilities;
 public class Check {
 
     public static void main(String[] args) {
-        System.out.println("Check");
+        System.out.println("Check 1");
 
         System.out.println("Check 2");
 
