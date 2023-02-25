aaf3645d41469a96e245658c1d6fb5c52ab2bb0c
Pc <raykurt@hotmail.com>
2023-02-25 12:30:32 +0300

    Commit_New

--- src/test/java/pages/AutomationPracticeRegister.java
+++ src/test/java/pages/AutomationPracticeRegister.java
@@ -73,13 +73,13 @@ public class AutomationPracticeRegister {
     public WebElement registerAccountButton;
 
     @FindBy(id="phone12")
-    public WebElement phoneNumberBox5;
+    public WebElement phoneNumberBox25;
 
     @FindBy(id="phone_mobile1")
-    public WebElement mobileNumberBox5;
+    public WebElement mobileNumberBox25;
 
     @FindBy(id="submitAccount1")
-    public WebElement registerAccountButton5;
+    public WebElement registerAccountButton25;
 
 
 }
